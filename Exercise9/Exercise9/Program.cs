86c02f03830688483b26463f51ec0cabec933925
SageTheAkuma-II <145461208+SageTheAkuma-II@users.noreply.github.com>
2024-03-05 10:19:38 -0500

    Update Program.cs

--- Exercise9/Exercise9/Program.cs
+++ Exercise9/Exercise9/Program.cs
@@ -4,7 +4,31 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //PROBLEM 1
+            //Prompt for width
+
+            //Prompt for number of rows
+
+            //Print correct # of asterisks
+            Console.Write("Enter row width ");
+            int rowWidth = int.Parse(Console.ReadLine());
+            {
+
+            }
+            Console.Write("Enter number of rows: ");
+            int numRows = int.Parse(Console.ReadLine());
+
+            // print rows
+            Console.WriteLine();
+            for (int i = 1; i <= numRows; i++)
+            {
+                for (int j = 1; j <= rowWidth; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
         }
     }
 }
