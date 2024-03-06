b13f906d816e579b6205c8d57421ddc5172e3b78
SageTheAkuma-II <145461208+SageTheAkuma-II@users.noreply.github.com>
2024-03-06 09:49:20 -0500

    Update Program.cs

--- Exercise9/Exercise9/Program.cs
+++ Exercise9/Exercise9/Program.cs
@@ -29,6 +29,23 @@
                 Console.WriteLine();
             }
 
+
+            //PROBLEM 2
+            Console.Write("How many astrisks? ");
+             rowWidth = int.Parse(Console.ReadLine());
+            
+
+
+            for (int i = 1; i <= rowWidth; i++) 
+            {
+                //Repeat for each row
+                for (int j = 1; j <= i; j++)
+                {
+                    //Repeats for each char
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }   
         }
     }
 }
